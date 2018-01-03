e32ed48104ab085a5a914cc14cebad1c9810c6ae
Fazle Rabbi Ador <aujisti.ador@gmail.com>
2018-01-03 16:21:22 +0600

    db modified

--- db/db_e_commerce_website.sql
+++ db/db_e_commerce_website.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 03, 2018 at 08:56 AM
+-- Generation Time: Jan 03, 2018 at 11:20 AM
 -- Server version: 10.1.24-MariaDB
 -- PHP Version: 7.1.6
 
@@ -40,7 +40,8 @@ CREATE TABLE `tbl_admin` (
 --
 
 INSERT INTO `tbl_admin` (`admin_id`, `admin_name`, `email_address`, `password`) VALUES
-(1, 'ador', 'admin@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b');
+(1, 'ador', 'admin@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b'),
+(2, 'razib', 'abc@gmail.com', '12345');
 
 --
 -- Indexes for dumped tables
@@ -60,7 +61,7 @@ ALTER TABLE `tbl_admin`
 -- AUTO_INCREMENT for table `tbl_admin`
 --
 ALTER TABLE `tbl_admin`
-  MODIFY `admin_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;COMMIT;
+  MODIFY `admin_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
